df7b8db18e62d3fb489adf190704a21678e1541a
b19karhj <sc2rena@gmail.com>
2025-03-07 11:05:43 +0100

    First module
    Core concepts of Angular

---
+++ Angular-Basic-Concepts/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+// import { NgFor, NgIf } from '@angular/common' NgFor och NgIf behövs i imports "Äldre version för dynamisk content";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  SelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
