3a4e2dd3ea025f38065ac1da2a700d11b3434b21
b19karhj <sc2rena@gmail.com>
2025-03-12 13:03:18 +0100

    App with service
    Using service in the app

--- Section-4/src/app/user-input/user-input.component.ts
+++ Section-4/src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,20 +10,21 @@ import type { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
-
   enteredInitialValue = signal('0');
   enteredAnnualValue = signal('0');
   enteredReturnValue = signal('0');
   enteredDurationValue = signal('0');
 
+  constructor(private investmentService: InvestmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialValue(),
       duration: +this.enteredDurationValue(),
       expectedReturn: +this.enteredReturnValue(),
       annualInvestment: +this.enteredAnnualValue(),
     });
+
     this.enteredInitialValue.set('0');
     this.enteredAnnualValue.set('0');
     this.enteredReturnValue.set('0');
