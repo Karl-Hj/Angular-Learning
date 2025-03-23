37e58403030423bc07cb893b105bb297f4453bed
b19karhj <sc2rena@gmail.com>
2025-03-23 09:30:47 +0100

    Update

--- Components-Templetes-Deep-Dive/src/app/dashboard/server-status/server-status.component.ts
+++ Components-Templetes-Deep-Dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,22 @@ import { Component, input } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  // Implements OnInit interfacet säkerställer att vi inte felstavar metoden.
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  //Simulate a random server value.
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
